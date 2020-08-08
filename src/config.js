28e04a3ae2602f356d2770a5afc4f33d219076e4
gdreid13 <gdreid13@gmail.com>
2020-08-08 02:48:18 -0700

    attempting full stack deployment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper',
-  TEST_DATABASE_URL: process.env.TEST.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper-test',
+  // TEST_DATABASE_URL: process.env.TEST.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper-test',
   JWT_SECRET: process.env.JWT_SECRET || 'not-so-secret',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "http://localhost:3000"

d32255de19df1ba15b451a1e26dd30f43bbed562
gdreid13 <gdreid13@gmail.com>
2020-08-08 02:35:40 -0700

    changing for heroku

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/hybowar-helper',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper',
+  TEST_DATABASE_URL: process.env.TEST.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper-test',
   JWT_SECRET: process.env.JWT_SECRET || 'not-so-secret',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
     "http://localhost:3000"

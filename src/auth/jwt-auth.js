eb9f9f6f4f4c7075a554f1cb62820b5c83e0153f
gdreid13 <gdreid13@gmail.com>
2020-09-02 16:15:41 -0700

    functioning

--- src/auth/jwt-auth.js
+++ src/auth/jwt-auth.js
@@ -2,7 +2,6 @@ const AuthService = require('../auth/auth-service')
 
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || ''
-
   let bearerToken
   if (!authToken.toLowerCase().startsWith('bearer ')) {
     return res.status(401).json({ error: 'Missing bearer token' })
@@ -12,9 +11,8 @@ function requireAuth(req, res, next) {
 
   try {
     const payload = AuthService.verifyJwt(bearerToken)
-
     AuthService.getUserWithUserName(
-      req.app.get('db')('hybowar_users'),
+      req.app.get('db'),
       payload.sub,
     )
       .then(user => {

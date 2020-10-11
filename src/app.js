455028efb34ecc5c80c30e6d97b35bb943e774a8
gdreid13 <gdreid13@gmail.com>
2020-10-10 22:59:03 -0700

    playing with cors

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
 const positionsRouter = require('./positions/positions-router')
 const charactersRouter = require('./characters/characters-router')
+const { CLIENT_ORIGIN } = require('./config');
 
 const app = express()
 
@@ -17,7 +18,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 
 app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ module.exports = {
   // TEST_DATABASE_URL: process.env.TEST.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper-test',
   JWT_SECRET: process.env.JWT_SECRET || 'not-so-secret',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:3000"
+    "http://localhost:3000",
+  CLIENT_ORIGIN: 'https://hybowar-client.vercel.app/'
 }

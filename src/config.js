98c103efe6608b5d569e202e84efd8e395218bd5
gdreid13 <gdreid13@gmail.com>
2020-10-10 23:07:34 -0700

    playing with cors and config

--- src/config.js
+++ src/config.js
@@ -4,7 +4,5 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper',
   // TEST_DATABASE_URL: process.env.TEST.DATABASE_URL || 'postgresql://postgres@localhost/hybowar-helper-test',
   JWT_SECRET: process.env.JWT_SECRET || 'not-so-secret',
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:3000",
-  CLIENT_ORIGIN: 'https://hybowar-client.vercel.app/'
+  CLIENT_ORIGIN: 'https://hybowar-client.vercel.app'
 }

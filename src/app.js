c0a4419a43d29364f1f29f146826e5761898fbe4
gdreid13 <gdreid13@gmail.com>
2020-10-10 22:25:48 -0700

    playing with cors

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.options('*', cors());
 
 app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
